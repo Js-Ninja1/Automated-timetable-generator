f713b49627a4969f81b062f45d3ede01e93d03e5
Gilbert kitetu <gilbertkitetu2@gmail.com>
2020-12-13 16:23:16 +0300

    First commit with admin UI

---
+++ admin/adminpage.php
@@ -0,0 +1,25 @@
+<?php include('../templates/header.php'); ?>
+
+<div id="page-wrapper">
+
+    <div class="container">
+
+        <!-- Page Heading -->
+        <div class="row" height="500" width="1000">
+
+
+            <!-- <img src="../images/admincontainer.jpg" height="500" width="500"> -->
+        </div>
+    </div>
+    <!-- /.row -->
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- /#page-wrapper -->
+
+</div>
+<!-- /#wrapper -->
+
+<?php include('../templates/footer.php') ?>
---
+++ templates/header.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Admin page</title>
+
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="../templates/header.css">
+
+    <!-- Custom Fonts-->
+ 
+    <link rel='icon' href='favicon.ico' type='image/x-icon'/>
+   
+   <script src="https://kit.fontawesome.com/c8879970d7.js" crossorigin="anonymous"></script>
+
+
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <nav class="topnav" role="navigation">
+
+            <div class="navbar-header">
+
+                <h2>Hello Admin
+                    <span style="float:right"><a href="logout.php" style="font-size: 20px; color: white;">Logout</a></span>
+</h2>
+            </div>
+
+            <!-- Top Menu Items -->
+
+
+            <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+            <div class="side-nav">
+                <ul class="side-nav-content">
+                    <li>
+                        <a href="index.html"><i class="fa fa-fw fa-dashboard"></i>Dashboard</a>
+                    </li>
+                    <li>
+                        <a href=""><i class="fa fa-fw fa-bar-chart-o"></i>Rooms</a>
+                    </li>
+                    <li>
+                        <a href=""><i class="fa fa-fw fa-table"></i>Course</a>
+                    </li>
+                    <li>
+                        <a href=""><i class="fa fa-fw fa-edit"></i>Lecture</a>
+                    </li>
+                    <li>
+                        <a href=""><i class="fa fa-fw fa-desktop"></i>Time frames</a>
+                    </li>
+
+
+
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </nav>

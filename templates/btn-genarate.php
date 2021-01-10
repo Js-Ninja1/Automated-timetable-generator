2e3ad8f5374b9334bae523ec33c3dbaefbc54a66
Gilbert kitetu <gilbertkitetu2@gmail.com>
2021-01-10 17:42:03 +0300

    scrabled

---
+++ templates/btn-genarate.php
@@ -0,0 +1,54 @@
+<style>
+    @import url('https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i');
+
+*
+{
+  margin: 0;
+  padding: 0;
+}
+p
+{
+  margin: 0 0 10px;
+}
+
+ul
+{
+  list-style: none;
+}
+.animate {
+  font-size: 50px;
+  margin: 100px 0 0;
+  border-bottom: 2px solid #ccc;
+}
+
+.animate span {
+  display: inline-block;
+}
+
+a.repeat {
+  display: inline-block;
+  font-size: 12px;
+  text-transform: none;
+  text-decoration: none;
+  color: orange;
+  padding: 5px 12px;
+  border: 1px solid rgba(0, 0, 0, 0.15);
+  font-weight: normal;
+  margin: 0 0 0 50px;
+  border-radius: 3px;
+  position: relative;
+  bottom: 15px;
+}
+
+a.repeat:hover {
+  background: rgba(0, 0, 0, 0.7);
+  color: white;
+}
+
+</style>
+<div class="animate five">
+      <span>c</span><span>s</span><span>s</span><span>3</span> &nbsp;
+      <span>a</span><span>n</span><span>i</span><span>m</span><span>a</span><span>t</span><span>i</span><span>o</span><span>n</span><span>s</span>
+      
+      <a class="repeat" href="javascript:void(0);">Repeat Animation</a>
+    </div>
--- templates/header.php
+++ templates/header.php
@@ -59,13 +59,46 @@ color: white;
         } 
 </style>
                 <h2>Hello Admin
-                <span id="btnGenerateTimetable" style="float:left"><a href="#" id="btnGenerateTimetable" style="font-size: 25px; padding-left:500px; color: white;  text-shadow: 0 1px 0 #ccc, 0 2px 0 #ccc, 
-                0 3px 0 #ccc, 0 4px 0 #ccc, 
-                0 5px 0 #ccc, 0 6px 0 #ccc, 
-                0 7px 0 #ccc, 0 8px 0 #ccc, 
-                0 9px 0 #ccc, 0 10px 0 #ccc, 
-                0 11px 0 #ccc, 0 12px 0 #ccc, 
-                0 20px 30px rgba(0, 0, 0, 0.5); ">Generate New Timetable</a></span>
+                <span id="btnGenerateTimetable" style="float:left"><a href="#" id="btnGenerateTimetable" style="font-size: 25px; padding-left:500px; color: white; border: black, solid, 2px;">Generate New Timetable</a></span>
+                <div class="console-container">
+                    <style>
+                        @import url(https://fonts.googleapis.com/css?family=Khula:700);
+body {
+  background: #111;
+}
+.hidden {
+  opacity:0;
+}
+.console-container {
+ 
+  font-family:Khula;
+  font-size:4em;
+  text-align:center;
+  height:200px;
+  width:600px;
+  display:block;
+  position:absolute;
+  color:white;
+  top:0;
+  bottom:0;
+  left:0;
+  right:0;
+  margin:auto;
+}
+.console.underscore{
+    display:inline-block;
+    position: relative;
+    top: -0.14em;
+    left: 10px;
+}
+                    </style>
+                    <script>
+                        
+                    </script>
+                    <span id="text"></span>
+                    <div class="console-underscore" id="console">$#95;</div>
+
+                </div>
                     <span style="float:right"><a href="logout.php" style="font-size: 20px; color: white; padding-right:10px">Logout</a></span>
 </h2>
             </div>
